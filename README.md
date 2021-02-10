da3abedfdc40aa1c0397a293696efdae4a6e5df3
Robert <3751418+ralexander-phi@users.noreply.github.com>
2021-02-09 23:14:35 -0500

    HTTP GET and image checks

---
+++ README.md
@@ -0,0 +1,75 @@
+# Client-side Host Reachability
+
+A simple, client-side javascript only, website reachability checker.
+
+Unlike most website monitoring tools, this project only performs checks from the user's web browser.
+There is no backend.
+This is helpful in situations where you're not able to use a [server-side monitoring tool](https://github.com/crazy-canux/awesome-monitoring).
+Those are much more full featured, so please consider those first.
+
+This repo auto deploys to GitHub Pages, you can see a demo here.
+
+## Setup
+
+To create your own list of monitored sites: fork this repo, customize the list of monitors, and deploy to your own GitHub pages site (or your preferred [static hosting](https://github.com/b-long/awesome-static-hosting-and-cms#free-hosting)).
+You shouldn't need to edit the HTML or javascript, just the JSON file.
+
+
+## Creating monitors
+
+Edit `monitors.json` to add the monitors you'd like to track.
+
+There are two types of monitors supported:
+* `http-get` - Performs an HTTP GET
+* `img` - Tries to load an image
+
+To add your monitor add a `name`, `url`, `check`, and any `params` as needed.
+See the sample `monitor.json` for details.
+
+
+### HTTP GET monitors
+
+This monitor will run an HTTP GET against the selected URL and check if the HTTP status code is in a list of acceptable codes.
+
+HTTP GET monitors are limited by [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS) and cannot work when CORS doesn't permit resources to load.
+You may need to log in to each site if the health endpoint requires a user authentication cookie.
+
+Example:
+```
+{
+  "name": "example",
+  "url": "https://example.com/health",
+  "check": "http-get",
+  "params": {
+    "permitted-status": [ 200, 204 ],
+  }
+}
+```
+
+### Image monitors
+
+The image monitor isn't restricted by CORS limitations, so it will work for more sites.
+
+The ideal image to monitor will be small (to keep data transfer low) and should be something unlikely to be removed by a future website update.
+favicons are a natural choice here.
+
+There are various failure cases where a site's images would be loadable but the site would be offline for users.
+As such, this simple check won't be comprehensive and is not ideal.
+
+Example:
+```
+{
+  "name": "example",
+  "url": "https://example.com/favicon.ico",
+  "check": "img",
+}
+```
+Note: `params` is not needed here.
+
+
+## Settings
+
+* `refreshSeconds` retry checks after a period of time
+
+Settings can be applied at the top level (to apply to all sites) or as an override for a per-site configuration.
+
---
+++ script.js
@@ -0,0 +1,104 @@
+function setStatus(element, statusClassName) {
+  element.classList.remove('up');
+  element.classList.remove('down');
+  element.classList.remove('checking');
+  element.classList.add(statusClassName);
+}
+
+function setup(config) {
+  var table = document.getElementById('checks');
+
+  var defaultRefresh = null;
+  if ('refreshSeconds' in config) {
+    defaultRefresh = config['refreshSeconds'];
+  }
+
+  for (idx in config['sites']) {
+    var row = table.insertRow(-1);
+    var nameCell = row.insertCell(0);
+    var statusCell = row.insertCell(1);
+    const name = config['sites'][idx]['name'];
+    const url = config['sites'][idx]['url'];
+
+    var siteRefresh = defaultRefresh;
+    if ('refreshSeconds' in config['sites'][idx]) {
+      siteRefresh = config['sites'][idx]['refreshSeconds'];
+    }
+
+    nameCell.innerText = name;
+    var statusElm = document.createElement('div');
+    statusCell.appendChild(statusElm);
+
+    var whichCheck = config['sites'][idx]['check'];
+    var params = null;
+    if ('params' in config['sites'][idx]) {
+      params = config['sites'][idx]['params'];
+    }
+
+    if ('img' == whichCheck) {
+      imgCheck(name, url, siteRefresh, statusElm);
+    } else if ('http-get' == whichCheck) {
+      httpGetCheck(name, url, siteRefresh, params, statusElm);
+    } else {
+      console.log('Unsure what to do for ' + name + ' ' + whichCheck);
+    }
+  }
+}
+
+function genImgCheck(name, url, siteRefresh, statusElm) {
+  return function() {
+    imgCheck(name, url, siteRefresh, statusElm);
+  }
+}
+
+function imgCheck(name, url, siteRefresh, statusElm) {
+  setStatus(statusElm, 'checking');
+  console.log('Image check: ' + name);
+
+  var img = document.createElement('img');
+  img.onload = function() {
+    setStatus(statusElm, 'up');
+  }
+  img.onerror = function() {
+    setStatus(statusElm, 'down');
+  }
+
+  // Burst the browser cache
+  img.src = url + '?' + Math.random();
+
+  setTimeout(genImgCheck(name, url, siteRefresh, statusElm), siteRefresh*1000);
+}
+
+function genHttpGetCheck(name, url, siteRefresh, params, statusElm) {
+  return function() {
+    httpGetCheck(name, url, siteRefresh, params, statusElm);
+  }
+}
+
+function httpGetCheck(name, url, siteRefresh, params, statusElm) {
+  setStatus(statusElm, 'checking');
+  console.log('HTTP GET check: ' + name);
+
+  fetch(url)
+  .then(response => { 
+    if (params['permitted-status'].includes(response.status)) {
+      setStatus(statusElm, 'up');
+    } else {
+      // status code not permitted
+      setStatus(statusElm, 'down');
+    }
+  })
+  .catch(reason=>{
+    // network error
+    setStatus(statusElm, 'down');
+  });
+
+  setTimeout(genHttpGetCheck(name, url, siteRefresh, params, statusElm), siteRefresh*1000);
+}
+
+window.onload = function() {
+  fetch('monitor.json')
+    .then(data=>{ return data.json()})
+    .then(config=>{setup(config)})
+}
+
